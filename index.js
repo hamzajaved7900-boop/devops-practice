74ac32ef55c8f73e9fcc96ffe5061276e0a02802
hamza-javed <aapka-email@example.com>
2026-09-02 12:58:03 +0500

    Fix syntax error in index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-res.send("<h1>CI/CD Pipeline Live Deployment Working Automatically!</h1>");
+const port = 3000;
+
 app.get('/', (req, res) => {
-    res.send('<h1>Docker App Version 2 - Running Successfully Allah is very great and be kind to human !</h1>');
+  res.send('<h1>CI/CD Pipeline Live Deployment Working Automatically!</h1>');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
