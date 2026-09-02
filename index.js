1ea4b4ccf0408a633cbfcbbd922f1a6febcd43e1
hamza-javed <aapka-email@example.com>
2026-09-02 13:15:37 +0500

    Update index.js message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>CI/CD Pipeline Live Deployment Working Automatically!</h1>');
+  res.send('<h1>CI/CD Pipeline Live Deployment Working Office Time </h1>');
 });
 
 app.listen(port, () => {
